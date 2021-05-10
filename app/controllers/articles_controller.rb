635713ff336ff04326cb8f137b694542b7839288
Thomas Marlowe <thomgarbe@gmail.com>
2021-05-10 13:48:23 -0400

    Added UPDATE functionality for editing existing articles in the blog. Added a link to each article for accessing the editing form. Both views for new and edit now share a partial _form. Still features error handling.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,20 +11,34 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-  def create
-    @article = Article.new(article_params)
-
 =begin
     redirect_to will cause the browser to make a new request, whereas render 
     renders the specified view for the current request. It is important to use 
     redirect_to after mutating the database or application state. Otherwise, 
     if the user refreshes the page, the browser will make the same request, 
-      and the mutation will be repeated.
+    and the mutation will be repeated.
 =end
+  def create
+    @article = Article.new(article_params)
+
     if @article.save
       redirect_to @article
     else
       render :new
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
     end
   end
 
